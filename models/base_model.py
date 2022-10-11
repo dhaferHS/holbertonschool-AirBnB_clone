abad98e55fd715a48a9d075e53e9566d9584c3f8
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-11 20:42:25 +0100

    2 commit

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,22 @@ import models
 class BaseModel:
     """ base class for instance """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        """intialisation of id abd the instance with datetime
+        args:
+            args: set for arguments 
+            kwargs: sts for the named arguments
+        """
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime().strftime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
 
     def __str__(self):
         """return tring with some information about thhe object"""
@@ -24,7 +36,7 @@ class BaseModel:
         """updates the public instance attribute with date"""
 
         self.updated_at = datetime.now()
-
+        
 
     def to_dict(self):
         """ return a dictionary containing al keys and values of dict of the instance"""
