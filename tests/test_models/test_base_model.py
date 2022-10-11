b7ddd89995c6308455d61fef2a455d88142fa70c
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-12 00:10:12 +0100

    3-push

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,10 @@ import pycodestyle
 from models.base_model import BaseModel
 
 
+class TestBaseModel(unittest.TestCase):
+    """test for pycodestyle"""
+    
+
 
 if __name__ == '__main__':
     unittest.main()
