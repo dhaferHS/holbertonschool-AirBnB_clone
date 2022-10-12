6f11e4d39c738cec24fd4b1871729245f141deb0
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-12 21:24:20 +0100

    4-push

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from datetime import datetime
+from models.user import user
 from models import storage
 import cmd
 import sys
