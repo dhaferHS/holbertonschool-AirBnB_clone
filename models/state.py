81873ce7b6858343a53e821a5dff32f26fc2cf35
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-13 00:09:09 +0100

    6-push

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!//usr/bin/python3
+"""impot modules"""
+
+from models.base_model import BaseModel
+
+"""class file for state"""
+
+
+class State(BaseModel):
+    """class user inherits from basemodel"""
+
+    name = ""
+
