6e81dff71ad4e7649c17cb8ecee21105c10a2dab
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-12 20:34:26 +0100

    4-push

--- console.py
+++ console.py
@@ -76,6 +76,49 @@ class HBNBCommand(cmd.Cmd):
                 del(new_dict[new_str])
                 storage.save()
                 
+    def do_all(self, arg):
+        """All command prints all string representation of all instances All command with a class prints the string representation of instances of this class"""
+        new_list = []
+        dict_obj = storage.all()
+
+        if not arg:
+            for key, value in dict_obj.items():
+                new_list.append(str(value))
+            print(new_list)
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in dict_obj.items():
+                if value.__class__.__name__ == arg:
+                    new_list.append(str(value))
+            print(new_list)
+
+    def do_update(self, arg):
+        """Update command updates an instance based on the class name and id"""
+        array = arg.split()
+        if len(array) < 1:
+            print("** class name missing **")
+            return
+        elif array[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(array) < 2:
+            print("** instance id missing **")
+            return
+        else:
+            new_dict = storage.all()
+            new_str = f"{array[0]}.{array[1]}"
+            if new_str not in new_dict.keys():
+                print("** no instance found **")
+            elif len(array) < 3:
+                print("** attribute name missing **")
+                return
+            elif len(array) < 4:
+                print("** value missing **")
+                return
+            else:
+                setattr(new_dict[new_str], array[2], array[3])
+                storage.save()            
  
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
