1ae7181c40fa201e2620995116206b34bd381b89
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-12 18:23:35 +0100

    4-push

--- console.py
+++ console.py
@@ -26,7 +26,37 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Empty line command"""
         pass
+    
+    def do_create(self, arg):
+        """Create command that creates a new instance"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(arg)()
+            new_obj.save()
+            print(new_obj.id)
 
+    def do_show(self, arg):
+        """Show command prints the string representation of an instance based on the class name and id"""
+        array = arg.split()
+
+        if len(array) < 1:
+            print("** class name missing **")
+        elif array[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(array) < 2:
+            print("** instance id missing **")
+        else:
+            new_dict = storage.all()
+            new_str = f"{array[0]}.{array[1]}"
+            if new_str not in new_dict.keys():
+                print("** no instance found **")
+            else:
+                print(new_dict[new_str])
+
+    
  
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
