8a3a56955d393f06a6b42d5d8bfdd6a14bfa7d0b
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-13 01:28:29 +0100

    5-push

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!//usr/bin/python3
+"""impot modules"""
+
+from models.base_model import BaseModel
+
+"""class file for amenity"""
+
+
+class Amenity(BaseModel):
+    """class user inherits from basemodel"""
+
+    name = ""
