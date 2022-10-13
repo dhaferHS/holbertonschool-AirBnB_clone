79a6ba52637506fe1c013f4509fdac297b2e506c
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-13 01:21:54 +0100

    5-push

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!//usr/bin/python3
+"""impot modules"""
+
+from models.base_model import BaseModel
+
+"""class file for city"""
+
+
+class City(BaseModel):
+    """class user inherits from basemodel"""
+
+    state_id = ""
+    name = ""
