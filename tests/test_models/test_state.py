671351eda7746305df4daf76819be98a8cfb6b78
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-13 19:52:24 +0100

    7-push

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest module for State"""
+from re import S
+from models.base_model import BaseModel
+from models.state import State
+import unittest
+import pycodestyle
+from datetime import datetime
+
+class TestState(unittest.TestCase):
+    """class testing the State class"""
+
+    def test_init(self):
+        """test the creation of an instance"""
+        s = State()
+        self.assertEqual(s.name, '')
+        s.name = 'dhafer'
+        self.assertEqual(s.name, 'dhafer')
+        self.assertTrue(hasattr(s, 'id'))
+        self.assertTrue(hasattr(s, 'created_at'))
+        self.assertTrue(hasattr(s, 'updated_at'))
+        self.assertTrue(issubclass(s.__class__, BaseModel))
+
+    def test_str(self):
+        """Test the string output of the class"""
+        s = State()
+        self.assertEqual(str(s), f"[State] ({s.id}) {s.__dict__}")
+
+
+if __name__ == '__main__':
+    unittest.main()
